d0c73a7836efe68adc41385da7e97358a1837474
Danny Baumann <dannybaumann@web.de>
2014-11-12 17:14:27 -0800

    Add back blacklist settings.
    Change-Id: Iae35717acfa9af4da75c07d24352acf981d19fc7
    
    Improve translatability of summaries.
    
    Change-Id: I362b335cbe219d6e371a458c9d50fcd66bdf19fe
    
    Settings: Alert user when they attempt to add an invalid number to BL.
    
      - BlacklistProvider will reject numbers that aren't valid, silently. So instead of
      resuming to a blank screen, we should alert the user that the number they attempted to
      add is invalid by utilizing BlacklistUtils#addOrUpdate method.
    
    Change-Id: I1ca32427baad41c1c9379e18046e6ca7937b6d32
    
    Add missing blacklist xxhdpi drawables.
    
    Change-Id: I327f408ff344e9358c479cf5fe9dc341918511b2
    
    Blacklist: Fix launching blacklist settings
    
    Change-Id: I0043503e6ac0d3536941394ee5057001b72f9114
    
    Settings : Launch only contacts picker for blacklist entry
    
    Change-Id: I266d1ad7f1aab745728b33490268cb197188f2b3

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -78,6 +78,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.chroma.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -355,7 +356,8 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),          
+            BlacklistSettings.class.getName()
     };
 
 
