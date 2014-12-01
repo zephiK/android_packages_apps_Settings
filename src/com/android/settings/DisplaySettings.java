18226bd22d498f7143dc23f6e9c1fe5cd81206ad
Danesh M <daneshm90@gmail.com>
2014-12-01 09:29:57 -0600

    Settings: Add proximity wake option
    Ports and combines:
    
    http://review.cyanogenmod.org/#/c/66658/
    Settings : Add preference for proximity wake
    
    http://review.cyanogenmod.org/#/c/69347/
    Proximity Wake-Up : adjust strings
    
    http://review.cyanogenmod.org/#/c/69803/
    Settings : Make proximity opt-in
    
    Change-Id: Iea0853eccf764438c7aaac76afd13c364fcea0ec
    
    Conflicts:
    	res/values/slim_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -84,6 +84,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_LCD_DENSITY = "lcd_density";
     private static final String KEY_WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -209,6 +210,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                         Settings.System.WAKEUP_WHEN_PLUGGED_UNPLUGGED, 1) == 1);
             mWakeUpWhenPluggedOrUnplugged.setOnPreferenceChangeListener(this);
         }
+        boolean proximityCheckOnWait = getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake);
+        if (!proximityCheckOnWait) {
+            //counter++;
+            //mWakeUpOptions.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+            Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
+        }
+
+        //if (counter == 3) {
+            //prefSet.removePreference(mWakeUpOptions);
+        //}
     }
 
     private static boolean allowAllRotations(Context context) {
