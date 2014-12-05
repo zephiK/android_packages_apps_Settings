8a9e42a3ed95ada3af3d4143795d7d3b359f30df
Roman Birg <roman@cyngn.com>
2014-12-05 15:54:34 +0000

    Utils: add isPackageInstalled boolean
    Conflicts:
    	src/com/android/settings/Utils.java
    
    Change-Id: If2b988138229e8db73859f1317b2c9e98bbcf808

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1138,4 +1138,23 @@ public final class Utils {
     public static boolean isTablet(Context con) {
         return getScreenType(con) == DEVICE_TABLET;
     }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
 }
