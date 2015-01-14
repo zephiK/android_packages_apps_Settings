305536fe7ef9d9c3fd8e5c0192ff0019a0b66cfa
Dave Kessler <activethrasher00@gmail.com>
2015-01-13 21:05:59 -0500

    Status Bar Clock: rewrite for lollipop and add left clock (2/2)
    Add clock mods to cm-12.0 with new left position
    
    [mikeioannina]: Adapt for CyanogenMod
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/status_bar_settings.xml
    	src/com/android/settings/rastapop/StatusBarSettings.java

--- src/com/android/settings/rastapop/StatusBarSettings.java
+++ src/com/android/settings/rastapop/StatusBarSettings.java
@@ -1,8 +1,13 @@
 package com.android.settings.rastapop;
 
 import android.content.ContentResolver;
+<<<<<<< HEAD:src/com/android/settings/rastapop/StatusBarSettings.java
 import android.database.ContentObserver;
 import android.net.Uri;
+=======
+import android.content.res.Configuration;
+import android.content.res.Resources;
+>>>>>>> eb755f5... Status Bar Clock: rewrite for lollipop and add left clock (2/2):src/com/android/settings/terminus/StatusBarSettings.java
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.ListPreference;
@@ -11,6 +16,8 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.text.format.DateFormat;
+import android.view.View;
 
 import com.android.settings.rastapop.qs.QSTiles;
 import com.android.settings.R;
@@ -25,12 +32,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     // Statusbar battery percent
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-
     private static final String TAG = "StatusBar";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_BATTERY_STYLE_HIDDEN = "4";
     private static final String STATUS_BAR_BATTERY_STYLE_TEXT = "6";
-	
+
     // Quick Pulldown
     private SwitchPreference mStatusBarQuickQsPulldown;
     // status bar brightness control
@@ -40,6 +46,20 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private ListPreference mStatusBarBatteryShowPercent;
     // customizable tiles
     private Preference mQSTiles;
+    public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
+    // Cendter clock
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+    // Statusbar battery percent
+    private ListPreference mStatusBarBattery;
+    private ListPreference mStatusBarBatteryShowPercent;
+    // Quick Pulldown
+    private SwitchPreference mStatusBarQuickQsPulldown;
+    // Center clock
+    private ListPreference mStatusBarClock;
+    private ListPreference mStatusBarAmPm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,10 +98,42 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                 .getApplicationContext().getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0) == 1));
         mStatusBarQuickQsPulldown.setOnPreferenceChangeListener(this);
-
 	// Quick Settings Tile Customization
 	mQSTiles = findPreference("qs_order");
 }
+        // Center clock
+        mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+        mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
+
+        int clockStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CLOCK, 1);
+        mStatusBarClock.setValue(String.valueOf(clockStyle));
+        mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        mStatusBarClock.setOnPreferenceChangeListener(this);
+
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        } else {
+            int statusBarAmPm = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_AM_PM, 2);
+            mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
+            mStatusBarAmPm.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Adjust clock position for RTL if necessary
+        Configuration config = getResources().getConfiguration();
+        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
+                        R.array.status_bar_clock_style_entries_rtl));
+                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        }
+    }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
     ContentResolver resolver = getActivity().getContentResolver();
@@ -116,6 +168,21 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, value ? 1 : 0);
             return true;
 		}
+        } else if (preference == mStatusBarClock) {
+            int clockStyle = Integer.parseInt((String) objValue);
+            int index = mStatusBarClock.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_CLOCK_STYLE, clockStyle);
+            mStatusBarClock.setSummary(mStatusBarClock.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarAmPm) {
+            int statusBarAmPm = Integer.valueOf((String) objValue);
+            int index = mStatusBarAmPm.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
@@ -133,5 +200,4 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mQSTiles.setSummary(getResources().getQuantityString(R.plurals.qs_tiles_summary,
                     qsTileCount, qsTileCount));
     }
-
 }
