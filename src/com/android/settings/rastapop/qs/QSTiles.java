46d549dcb21195e93a3f115157675a834d4856cc
zephiK <zephik@gmail.com>
2015-01-16 11:58:40 -0500

    Cleanup previous code

--- src/com/android/settings/rastapop/qs/QSTiles.java
+++ src/com/android/settings/rastapop/qs/QSTiles.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.settings.cyanogenmod.qs;
+package com.android.settings.rastapop.qs;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
