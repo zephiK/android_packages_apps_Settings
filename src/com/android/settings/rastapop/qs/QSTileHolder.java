d18602f0e96bf44f78cc78492ce72ed9d19afe33
lichti1901 <lichti190185@gmail.com>
2015-01-17 15:06:38 +0100

    make it match dark theme

--- src/com/android/settings/rastapop/qs/QSTileHolder.java
+++ src/com/android/settings/rastapop/qs/QSTileHolder.java
@@ -68,9 +68,6 @@ public class QSTileHolder {
         } else if (tileType.equals("hotspot")) {
             resId = R.drawable.ic_qs_hotspot;
             stringId = R.string.hotspot;
-        } else if (tileType.equals("notifications")) {
-            resId = R.drawable.ic_qs_ringer_audible;
-            stringId = R.string.notifications;
         } else if (tileType.equals("data")) {
             resId = R.drawable.ic_qs_data_on;
             stringId = R.string.mobile_data;
