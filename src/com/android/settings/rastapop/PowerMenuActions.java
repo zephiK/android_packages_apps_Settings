d1902a5cc7b0b16a7fc24fe727e635df2091552f
zephiK <zephik@gmail.com>
2015-01-16 22:15:21 -0500

    Rename files

--- src/com/android/settings/rastapop/PowerMenuActions.java
+++ src/com/android/settings/rastapop/PowerMenuActions.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.cyanogenmod;
+package com.android.settings.rastapop;
 
 import android.content.Context;
 import android.content.Intent;
