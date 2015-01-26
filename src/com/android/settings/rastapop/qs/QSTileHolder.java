65c56d24e510a95e27afeff0d8805260c93ebf04
Roman Birg <roman@cyngn.com>
2015-01-26 13:39:19 -0800

    Settings: add AudioFX tile
    Change-Id: Ieeda7fb826c2dd50e4ef50609d3357eb7190aba3
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	res/values/cm_strings.xml

--- src/com/android/settings/rastapop/qs/QSTileHolder.java
+++ src/com/android/settings/rastapop/qs/QSTileHolder.java
@@ -90,6 +90,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_zen_on";
                 stringId = R.string.qs_tile_notifications;
                 break;
+            case QSConstants.TILE_VISUALIZER:
+                resourceName = "ic_qs_visualizer_static";
+                stringId = R.string.qs_tile_visualizer;
+                break;
             default:
                 return null;
         }
