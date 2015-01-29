e77fcfd98b16bb3d8f81d3b3e3200c9416778a48
zephiK <zephik@gmail.com>
2015-01-28 22:22:00 -0500

    Change locations

--- src/com/android/settings/rastapop/BatterySettings.java
+++ src/com/android/settings/rastapop/BatterySettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.simpleaosp;
+package com.android.settings.rastapop;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
