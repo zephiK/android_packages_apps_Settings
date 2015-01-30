5a02da0873f49fd12e9c66a81caab0f2852f45aa
MrBaNkS <banksmi09@gmail.com>
2015-01-30 06:38:38 -0600

    QSHeadsUp tile settings: use native sound tile drawable

--- src/com/android/settings/rastapop/qs/QSTileHolder.java
+++ src/com/android/settings/rastapop/qs/QSTileHolder.java
@@ -121,7 +121,7 @@ public class QSTileHolder {
                 stringId = R.string.qs_tile_visualizer;
                 break;
 	    case QSConstants.TILE_HEADS_UP:
-                resourceName = "ic_qs_heads_up_on";
+                resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_headsup;
                 break;
             default:
