838a9717ebbaeafe3ea4baffdcd940ef901c2d7a
zephiK <zephik@gmail.com>
2015-01-30 20:40:07 -0500

    Compilation fixes

--- src/com/android/settings/rastapop/qs/QSTileHolder.java
+++ src/com/android/settings/rastapop/qs/QSTileHolder.java
@@ -92,43 +92,6 @@ public class QSTileHolder {
                 break;
             default:
                 return null;
-
-        if (tileType.equals("wifi")) {
-            resId = R.drawable.ic_settings_wireless;
-            stringId = R.string.wifi_quick_toggle_title;
-        } else if (tileType.equals("bt")) {
-            resId = R.drawable.ic_settings_bluetooth2;
-            stringId = R.string.bluetooth_settings_title;
-        } else if (tileType.equals("inversion")) {
-            resId = R.drawable.ic_settings_accessibility;
-            stringId = R.string.accessibility_display_inversion_preference_title;
-        } else if (tileType.equals("cell")) {
-            resId = R.drawable.ic_qs_signal;
-            stringId = R.string.cellular_data_title;
-        } else if (tileType.equals("airplane")) {
-            resId = R.drawable.ic_qs_airplane;
-            stringId = R.string.airplane_mode;
-        } else if (tileType.equals("rotation")) {
-            resId = R.drawable.ic_qs_rotation;
-            stringId = R.string.display_rotation_title;
-        } else if (tileType.equals("flashlight")) {
-            resId = R.drawable.ic_qs_flashlight;
-            stringId = R.string.power_flashlight;
-        } else if (tileType.equals("location")) {
-            resId = R.drawable.ic_settings_location;
-            stringId = R.string.location_title;
-        } else if (tileType.equals("cast")) {
-            resId = R.drawable.ic_qs_cast;
-            stringId = R.string.cast_screen;
-        } else if (tileType.equals("hotspot")) {
-            resId = R.drawable.ic_qs_hotspot;
-            stringId = R.string.hotspot;
-        } else if (tileType.equals("notifications")) {
-            resId = R.drawable.ic_qs_ringer_audible;
-            stringId = R.string.notifications;
-        } else if (tileType.equals("data")) {
-            resId = R.drawable.ic_qs_data_on;
-            stringId = R.string.mobile_data;
         }
 
         String name = stringId != -1 ? context.getString(stringId) : null;
