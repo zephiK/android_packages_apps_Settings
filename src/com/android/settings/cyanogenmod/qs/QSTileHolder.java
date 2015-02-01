292107f2774889df94148f67c76a0abc51261b7b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    QS: add Sync tile (2/2)
    Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d
    
    Conflicts:
    	res/values/strings.xml
    
    Conflicts:
    	res/values/strings.xml
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -90,6 +90,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_notifications;
                 break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_sync_tile;
+                break;
             default:
                 return null;
         }
