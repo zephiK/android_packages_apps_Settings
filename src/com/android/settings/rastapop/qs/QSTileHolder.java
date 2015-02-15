7c8ef9f20c2305645f0c1c43f73655dadbbffd3d
zephiK <zephik@gmail.com>
2015-02-14 20:04:44 -0500

    Add screen timeout tile

--- src/com/android/settings/rastapop/qs/QSTileHolder.java
+++ src/com/android/settings/rastapop/qs/QSTileHolder.java
@@ -124,6 +124,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_expanded_desktop";
                 stringId = R.string.qs_expanded_desktop_tile;
                 break;
+            case QSConstants.TILE_SCREEN_TIMEOUT:
+                resourceName = "ic_qs_screen_timeout_vector";
+                stringId = R.string.qs_tile_screen_timeout;
+                break;
             default:
                 return null;
         }
