21717dd865740b6b21a2041d65b5f5e6d1024c96
snak3ater <saeedyasir91@gmail.com>
2015-03-03 19:18:08 +0500

    Update ringer drawable [2/2]

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -167,7 +167,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
 
     private void updateRingIcon(int progress) {
         mRingPreference.showIcon(progress > 0
-                    ? R.drawable.ic_audio_ring_24dp
+                    ? R.drawable.ring_ring
                     : (mVibrator == null
                             ? R.drawable.ring_notif_mute
                             : R.drawable.ring_notif_vibrate));
