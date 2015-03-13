0eccc8eae74924f3c3d31566753ccc765747dab7
zephiK <zephik@gmail.com>
2015-03-13 18:02:22 -0400

    Add kill app to Navigation Bar menu

--- src/com/android/settings/chroma/NavigationBarSettings.java
+++ src/com/android/settings/chroma/NavigationBarSettings.java
@@ -13,13 +13,19 @@ import com.android.settings.SettingsPreferenceFragment;
 public class NavigationBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-		// private variables here
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
++    private SwitchPreference mShowAllANRs;
++    private SwitchPreference mKillAppLongpressBack;
 		
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.chroma_settings_navigation);
+
+        mResetCbPrefs.add(mShowAllANRs);
+        mKillAppLongpressBack = findAndInitCheckboxPref(KILL_APP_LONGPRESS_BACK);
     }
 
     @Override
@@ -27,5 +33,28 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
 
 		// preference changes here
         return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+    {
+        if (preference == mKillAppLongpressBack) {
+            writeKillAppLongpressBackOptions();
+        }
+    }
+
+    public void onResume()
+    {
+        updateKillAppLongpressBackOptions();
+    }
+    private void writeKillAppLongpressBackOptions() {
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.KILL_APP_LONGPRESS_BACK,
+                mKillAppLongpressBack.isChecked() ? 1 : 0);
+    }
+
+    private void updateKillAppLongpressBackOptions() {
+        mKillAppLongpressBack.setChecked(Settings.Secure.getInt(
+            getActivity().getContentResolver(), Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) != 0);
     }
 }
