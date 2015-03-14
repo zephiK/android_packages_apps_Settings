b426836fd1b16f7d196e73ff5e25a6a2bbf5c679
zephiK <zephik@gmail.com>
2015-03-14 02:51:03 -0400

    add navigation height

--- src/com/android/settings/chroma/NavigationBarSettings.java
+++ src/com/android/settings/chroma/NavigationBarSettings.java
@@ -20,8 +20,10 @@ import com.android.settings.SettingsPreferenceFragment;
 public class NavigationBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+    private static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
 
     private SwitchPreference mKillAppLongPressBack;
+    private ListPreference mNavigationBarHeight;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +37,14 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
                 KILL_APP_LONGPRESS_BACK, 0);
         mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+
+        // navigation bar height
+        mNavigationBarHeight = (ListPreference) findPreference(NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setOnPreferenceChangeListener(this);
+        int statusNavigationBarHeight = Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
+        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
     }
 
     @Override
@@ -44,6 +54,14 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), KILL_APP_LONGPRESS_BACK,
                     value ? 1 : 0);
+            return true;
+        } else if (preference == mNavigationBarHeight) {
+            // navigation bar height
+            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
+            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getContentResolver(), NAVIGATION_BAR_HEIGHT,
+                    statusNavigationBarHeight);
+            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
             return true;
         }
         return false;
