3b910b03680ef4c9bfd3e03c1179cbde89c11ea8
zephiK <zephik@gmail.com>
2015-03-27 01:16:39 -0400

    Remove notifications tile

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -86,10 +86,6 @@ public class QSTileHolder {
                 resourceName = "ic_qs_hotspot_on";
                 stringId = R.string.qs_tile_hotspot;
                 break;
-            case QSConstants.TILE_NOTIFICATIONS:
-                resourceName = "ic_qs_ringer_audible";
-                stringId = R.string.qs_tile_notifications;
-                break;
             case QSConstants.TILE_SCREEN_TIMEOUT:
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
