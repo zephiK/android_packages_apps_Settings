d123aa19b0ba1b4202dc41c1a97a150177d7aa88
zephiK <zephik@gmail.com>
2015-04-04 04:07:57 -0400

    Reimplement volume wake + skip track Reimplement the whole set of commits to fix a bug where volume wake will always remain on when ambient display is enabled

--- src/com/android/settings/chroma/VolumeRockerSettings.java
+++ src/com/android/settings/chroma/VolumeRockerSettings.java
@@ -13,17 +13,14 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
-    public static final String VOLUME_MUSIC_CONTROLS = "volume_music_controls";
+    private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
+    private static final String KEY_VOLBTN_MUSIC_CTRL = "volbtn_music_controls";
     private static final String VOLUME_KEY_ADJUST_SOUND = "volume_key_adjust_sound";
-    private static final String KEY_VOLUME_MUSIC_CONTROLS = "volbtn_music_controls";
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
-    private SwitchPreference mVolumeRockerWake;
+    private SwitchPreference mVolumeWake;
     private SwitchPreference mVolumeMusicControl;
     private SwitchPreference mVolumeKeyAdjustSound;
-    private SwitchPreference mVolumeWakeScreen;
-    private SwitchPreference mVolumeMusicControls;
     private ListPreference mVolumeKeyCursorControl;
 
     @Override
@@ -32,36 +29,23 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.chroma_settings_volume);
 
-        // volume rocker wake
-        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
-        mVolumeRockerWake.setOnPreferenceChangeListener(this);
-        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
-                VOLUME_ROCKER_WAKE, 0);
-        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
+        // volume wake options
+        mVolumeWake = (SwitchPreference) findPreference(KEY_VOLUME_WAKE);
+        mVolumeWake.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
+        mVolumeWake.setOnPreferenceChangeListener(this);
 
         // volume rocker music control
-        mVolumeMusicControl = (SwitchPreference) findPreference(VOLUME_MUSIC_CONTROLS);
+        mVolumeMusicControl = (SwitchPreference) findPreference(KEY_VOLBTN_MUSIC_CTRL);
+        mVolumeMusicControl.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.VOLUME_MUSIC_CONTROLS, 1) != 0);
         mVolumeMusicControl.setOnPreferenceChangeListener(this);
-        int volumeMusicControl = Settings.System.getInt(getContentResolver(),
-                VOLUME_MUSIC_CONTROLS, 1);
-        mVolumeMusicControl.setChecked(volumeMusicControl != 0);
 
         // volume key adjust sound
         mVolumeKeyAdjustSound = (SwitchPreference) findPreference(VOLUME_KEY_ADJUST_SOUND);
         mVolumeKeyAdjustSound.setOnPreferenceChangeListener(this);
         mVolumeKeyAdjustSound.setChecked(Settings.System.getInt(getContentResolver(),
                 VOLUME_KEY_ADJUST_SOUND, 1) != 0);
-
-        // wake screen dependency
-        mVolumeWakeScreen = (SwitchPreference) findPreference(Settings.System.VOLUME_ROCKER_WAKE);
-        mVolumeMusicControls = (SwitchPreference) findPreference(VOLUME_MUSIC_CONTROLS);
-
-        if (mVolumeWakeScreen != null) {
-            if (mVolumeMusicControls != null) {
-                mVolumeMusicControls.setDependency(Settings.System.VOLUME_ROCKER_WAKE);
-                mVolumeWakeScreen.setDisableDependentsState(true);
-            }
-        }
 
         // volume cursor control
 	    mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
@@ -75,17 +59,7 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mVolumeRockerWake) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mVolumeMusicControl) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(getContentResolver(), VOLUME_MUSIC_CONTROLS,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mVolumeKeyAdjustSound) {
+            if (preference == mVolumeKeyAdjustSound) {
             boolean value = (Boolean) objValue;
             Settings.System.putInt(getContentResolver(), VOLUME_KEY_ADJUST_SOUND,
                     value ? 1 : 0);
@@ -98,7 +72,18 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             int volumeKeyCursorControlIndex = mVolumeKeyCursorControl.findIndexOfValue(volumeKeyCursorControl);
             mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
             return true;
-            }
+            } else if (preference == mVolumeWake) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_WAKE_SCREEN,
+                    (Boolean) value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeMusicControl) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.VOLUME_MUSIC_CONTROLS,
+                    (Boolean) objValue ? 1 : 0);
+            return true;
+        }
         return false;
     }
 }
