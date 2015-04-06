SimpleAOSP-Lollipop/platform_packages_apps_Settings

Rotation tile: Longclick action [2/2]

src/com/android/settings/Settings.java
@@ -18,6 +18,7 @@ package com.android.settings;
 
 import com.android.settings.applications.AppOpsSummary;
 import com.android.settings.blacklist.BlacklistSettings;
+import com.android.settings.simpleaosp.DisplayRotation;
 
 /**
  * Top-level Settings activity
@@ -103,5 +104,6 @@ public class Settings extends SettingsActivity {
     public static class TopLevelSettings extends SettingsActivity { /* empty */ }
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -113,6 +113,7 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
+import com.android.settings.simpleaosp.DisplayRotation;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -299,7 +300,8 @@ public class SettingsActivity extends Activity
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
-            BlacklistSettings.class.getName()
+            BlacklistSettings.class.getName(),
+	    DisplayRotation.class.getName()
     };
 
 

zephiK/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java

src/com/android/settings/Settings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import com.android.settings.applications.AppOpsCategory;
+import com.android.settings.chroma.DisplayRotation;
 
 /**
  * Top-level Settings activity
@@ -102,5 +103,6 @@ public class Settings extends SettingsActivity {
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
 }
 

src/com/android/settings/SettingsActivity.java
@@ -114,6 +114,7 @@ import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.headsup.HeadsUpSettings;
+import com.android.settings.chroma.DisplayRotation;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -306,7 +307,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
-    	    HeadsUpSettings.class.getName()
+    	    HeadsUpSettings.class.getName(),
+    	    DisplayRotation.class.getName()
     };
 
 