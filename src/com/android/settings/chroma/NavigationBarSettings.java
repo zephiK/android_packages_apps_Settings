13357074d9ca3211d87628416e81f84a4e1ddc1b
zephiK <zephik@gmail.com>
2015-04-07 02:58:05 -0400

    Clear Recents: default to top right

--- src/com/android/settings/chroma/NavigationBarSettings.java
+++ src/com/android/settings/chroma/NavigationBarSettings.java
@@ -63,7 +63,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         mRecentsClearAll.setOnPreferenceChangeListener(this);
         mRecentsClearAllLocation = (ListPreference) prefSet.findPreference(RECENTS_CLEAR_ALL_LOCATION);
         int location = Settings.System.getIntForUser(resolver,
-                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 2, UserHandle.USER_CURRENT);
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 0, UserHandle.USER_CURRENT);
         mRecentsClearAllLocation.setValue(String.valueOf(location));
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
         updateRecentsLocation(location);
