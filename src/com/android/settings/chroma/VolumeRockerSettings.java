5e6f3e3997f52e142f902edc57e56d605282bfbf
zephiK <zephik@gmail.com>
2015-06-12 18:32:10 -0400

    Volume rocker cursor control Part 2 of Volume key cursor control by rascarlo - https://github.com/zephiK/android_frameworks_base/commit/94b2da452b4ec614489ee27e5c5593c8a266fde6 Credits to Rascarlo

--- src/com/android/settings/chroma/VolumeRockerSettings.java
+++ src/com/android/settings/chroma/VolumeRockerSettings.java
@@ -13,19 +13,39 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-		// private variables here
-		
+    private static String VOLUME_ROCKER_SETTINGS_CATEGORY = "volume_rocker_settings_category";
+    private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+    private ListPreference mVolumeKeyCursorControl;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.chroma_settings_navigation);
+
+	// volume cursor control
+	mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
+        if (mVolumeKeyCursorControl != null) {
+            mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+            mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                    .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
+            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
-		// preference changes here
+    if (preference == mVolumeKeyCursorControl) {
+            String volumeKeyCursorControl = (String) value;
+            int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, volumeKeyCursorControlValue);
+            int volumeKeyCursorControlIndex = mVolumeKeyCursorControl.findIndexOfValue(volumeKeyCursorControl);
+            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
+            return true;
+    }
         return false;
     }
 }
