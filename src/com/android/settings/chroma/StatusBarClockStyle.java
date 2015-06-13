014ab4ba8c1fc1fb43f7db5f16a99bb6aa353892
zephiK <zephik@gmail.com>
2015-06-13 11:10:03 -0400

    Reorganize files part 2

--- src/com/android/settings/chroma/StatusBarClockStyle.java
+++ src/com/android/settings/chroma/StatusBarClockStyle.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.simpleaosp;
+package com.android.settings.chroma;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
