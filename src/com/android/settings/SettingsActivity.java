Blazing-Rom/platform_packages_apps_Settings

Allow launching headsup settings externally
Conflicts:
	src/com/android/settings/SettingsActivity.java

Change-Id: I7cc28ed7eace00561c7fffea78faceb9d05f5426

src/com/android/settings/SettingsActivity.java
@@ -75,6 +75,7 @@ import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.simpleaosp.AdditionalSettings;
+import com.android.settings.headsup.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -308,7 +309,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),         
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+	    HeadsUpSettings.class.getName()
     };
 
 

zephiK/android_packages_apps_Settings

	src/com/android/settings/Settings.java

src/com/android/settings/SettingsActivity.java
@@ -74,6 +74,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.chroma.DisplayRotation;
+import com.android.settings.headsup.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -305,7 +306,8 @@ public class SettingsActivity extends Activity
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),      
-            DisplayRotation.class.getName()
+            DisplayRotation.class.getName(),
+    	    HeadsUpSettings.class.getName()
     };
 
 