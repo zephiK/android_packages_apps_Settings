snak3ater/platform_packages_apps_Settings

Privacy Guard Slim version (2/2) (Squshed)
Re-implements slims ehanced privacy guard from 4.3

Conflicts:
	res/values/strings.xml
	res/xml/security_settings_misc.xml

Change-Id: I24304aa50d0e699efb9059f695a81c6b3537baff

src/com/android/settings/simpleaosp/privacyguard/PrivacyGuardAppListAdapter.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2013 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.simpleaosp.privacyguard;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.simpleaosp.privacyguard.PrivacyGuardManager.AppInfo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PrivacyGuardAppListAdapter extends BaseAdapter implements SectionIndexer {
+
+    private LayoutInflater mInflater;
+    private PackageManager mPm;
+
+    private List<AppInfo> mApps;
+    private String[] mSections;
+    private int[] mPositions;
+    private ConcurrentHashMap<String, Drawable> mIcons;
+    private Drawable mDefaultImg;
+
+    private Context mContext;
+
+    //constructor
+    public PrivacyGuardAppListAdapter(Context context, List<AppInfo> apps,
+            List<String> sections, List<Integer> positions) {
+        mContext = context;
+        mInflater = LayoutInflater.from(mContext);
+        mPm = context.getPackageManager();
+
+        mApps = apps;
+        mSections = sections.toArray(new String[sections.size()]);
+        mPositions = new int[positions.size()];
+        for (int i = 0; i < positions.size(); i++) {
+            mPositions[i] = positions.get(i);
+        }
+
+        // set the default icon till the actual app icon is loaded in async task
+        mDefaultImg = mContext.getResources().getDrawable(android.R.mipmap.sym_def_app_icon);
+        mIcons = new ConcurrentHashMap<String, Drawable>();
+
+        new LoadIconsTask().execute(apps.toArray(new PrivacyGuardManager.AppInfo[]{}));
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PrivacyGuardAppViewHolder appHolder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.privacy_guard_manager_list_row, null);
+
+            // creates a ViewHolder and children references
+            appHolder = new PrivacyGuardAppViewHolder();
+            appHolder.title = (TextView) convertView.findViewById(R.id.app_title);
+            appHolder.icon = (ImageView) convertView.findViewById(R.id.app_icon);
+            appHolder.privacyGuardIcon = (ImageView) convertView.findViewById(R.id.app_privacy_guard_icon);
+            convertView.setTag(appHolder);
+        } else {
+            appHolder = (PrivacyGuardAppViewHolder) convertView.getTag();
+        }
+
+        PrivacyGuardManager.AppInfo app = mApps.get(position);
+
+        appHolder.title.setText(app.title);
+
+        Drawable icon = mIcons.get(app.packageName);
+        appHolder.icon.setImageDrawable(icon != null ? icon : mDefaultImg);
+
+        appHolder.privacyGuardIcon.setImageResource(
+            AppOpsManager.getPrivacyGuardIconResId(app.privacyGuardState, true));
+
+        return convertView;
+    }
+
+    @Override
+    public int getPositionForSection(int section) {
+        if (section < 0 || section >= mSections.length) {
+            return -1;
+        }
+
+        return mPositions[section];
+    }
+
+    @Override
+    public int getSectionForPosition(int position) {
+        if (position < 0 || position >= getCount()) {
+            return -1;
+        }
+
+        int index = Arrays.binarySearch(mPositions, position);
+
+        /*
+         * Consider this example: section positions are 0, 3, 5; the supplied
+         * position is 4. The section corresponding to position 4 starts at
+         * position 3, so the expected return value is 1. Binary search will not
+         * find 4 in the array and thus will return -insertPosition-1, i.e. -3.
+         * To get from that number to the expected value of 1 we need to negate
+         * and subtract 2.
+         */
+        return index >= 0 ? index : -index - 2;
+    }
+
+    @Override
+    public Object[] getSections() {
+        return mSections;
+    }
+
+    /**
+     * An asynchronous task to load the icons of the installed applications.
+     */
+    private class LoadIconsTask extends AsyncTask<PrivacyGuardManager.AppInfo, Void, Void> {
+        @Override
+        protected Void doInBackground(PrivacyGuardManager.AppInfo... apps) {
+            for (PrivacyGuardManager.AppInfo app : apps) {
+                try {
+                    Drawable icon = mPm.getApplicationIcon(app.packageName);
+                    mIcons.put(app.packageName, icon);
+                    publishProgress();
+                } catch (PackageManager.NameNotFoundException e) {
+                    // ignored; app will show up with default image
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... progress) {
+            notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * App view holder used to reuse the views inside the list.
+     */
+    public static class PrivacyGuardAppViewHolder {
+        TextView title;
+        ImageView icon;
+        ImageView privacyGuardIcon;
+    }
+}

src/com/android/settings/simpleaosp/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,104 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.simpleaosp.privacyguard;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+    private static final String KEY_PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+    private SwitchPreference mPrivacyGuardDefault;
+    private SwitchPreference mPrivacyGuardNotification;
+
+    public static PrivacyGuardPrefs newInstance() {
+        PrivacyGuardPrefs privacyGuardFragment = new PrivacyGuardPrefs();
+        return privacyGuardFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+
+        mPrivacyGuardDefault = (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardDefault.setChecked(false);
+        }
+
+        mPrivacyGuardNotification =
+            (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_NOTIFICATION);
+        mPrivacyGuardNotification.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardNotification.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardNotification.setChecked(true);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPrivacyGuardNotification) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

zephiK/android_packages_apps_Settings

	res/layout/installed_app_details.xml
	res/xml/security_settings_app_cyanogenmod.xml

src/com/android/settings/chroma/privacyguard/PrivacyGuardAppListAdapter.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2013 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.chroma.privacyguard;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.SectionIndexer;
+import android.widget.TextView;
+
+import com.android.settings.R;
+import com.android.settings.chroma.privacyguard.PrivacyGuardManager.AppInfo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PrivacyGuardAppListAdapter extends BaseAdapter implements SectionIndexer {
+
+    private LayoutInflater mInflater;
+    private PackageManager mPm;
+
+    private List<AppInfo> mApps;
+    private String[] mSections;
+    private int[] mPositions;
+    private ConcurrentHashMap<String, Drawable> mIcons;
+    private Drawable mDefaultImg;
+
+    private Context mContext;
+
+    //constructor
+    public PrivacyGuardAppListAdapter(Context context, List<AppInfo> apps,
+            List<String> sections, List<Integer> positions) {
+        mContext = context;
+        mInflater = LayoutInflater.from(mContext);
+        mPm = context.getPackageManager();
+
+        mApps = apps;
+        mSections = sections.toArray(new String[sections.size()]);
+        mPositions = new int[positions.size()];
+        for (int i = 0; i < positions.size(); i++) {
+            mPositions[i] = positions.get(i);
+        }
+
+        // set the default icon till the actual app icon is loaded in async task
+        mDefaultImg = mContext.getResources().getDrawable(android.R.mipmap.sym_def_app_icon);
+        mIcons = new ConcurrentHashMap<String, Drawable>();
+
+        new LoadIconsTask().execute(apps.toArray(new PrivacyGuardManager.AppInfo[]{}));
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PrivacyGuardAppViewHolder appHolder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.privacy_guard_manager_list_row, null);
+
+            // creates a ViewHolder and children references
+            appHolder = new PrivacyGuardAppViewHolder();
+            appHolder.title = (TextView) convertView.findViewById(R.id.app_title);
+            appHolder.icon = (ImageView) convertView.findViewById(R.id.app_icon);
+            appHolder.privacyGuardIcon = (ImageView) convertView.findViewById(R.id.app_privacy_guard_icon);
+            convertView.setTag(appHolder);
+        } else {
+            appHolder = (PrivacyGuardAppViewHolder) convertView.getTag();
+        }
+
+        PrivacyGuardManager.AppInfo app = mApps.get(position);
+
+        appHolder.title.setText(app.title);
+
+        Drawable icon = mIcons.get(app.packageName);
+        appHolder.icon.setImageDrawable(icon != null ? icon : mDefaultImg);
+
+        appHolder.privacyGuardIcon.setImageResource(
+            AppOpsManager.getPrivacyGuardIconResId(app.privacyGuardState, true));
+
+        return convertView;
+    }
+
+    @Override
+    public int getPositionForSection(int section) {
+        if (section < 0 || section >= mSections.length) {
+            return -1;
+        }
+
+        return mPositions[section];
+    }
+
+    @Override
+    public int getSectionForPosition(int position) {
+        if (position < 0 || position >= getCount()) {
+            return -1;
+        }
+
+        int index = Arrays.binarySearch(mPositions, position);
+
+        /*
+         * Consider this example: section positions are 0, 3, 5; the supplied
+         * position is 4. The section corresponding to position 4 starts at
+         * position 3, so the expected return value is 1. Binary search will not
+         * find 4 in the array and thus will return -insertPosition-1, i.e. -3.
+         * To get from that number to the expected value of 1 we need to negate
+         * and subtract 2.
+         */
+        return index >= 0 ? index : -index - 2;
+    }
+
+    @Override
+    public Object[] getSections() {
+        return mSections;
+    }
+
+    /**
+     * An asynchronous task to load the icons of the installed applications.
+     */
+    private class LoadIconsTask extends AsyncTask<PrivacyGuardManager.AppInfo, Void, Void> {
+        @Override
+        protected Void doInBackground(PrivacyGuardManager.AppInfo... apps) {
+            for (PrivacyGuardManager.AppInfo app : apps) {
+                try {
+                    Drawable icon = mPm.getApplicationIcon(app.packageName);
+                    mIcons.put(app.packageName, icon);
+                    publishProgress();
+                } catch (PackageManager.NameNotFoundException e) {
+                    // ignored; app will show up with default image
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... progress) {
+            notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * App view holder used to reuse the views inside the list.
+     */
+    public static class PrivacyGuardAppViewHolder {
+        TextView title;
+        ImageView icon;
+        ImageView privacyGuardIcon;
+    }
+}

src/com/android/settings/chroma/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,104 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.chroma.privacyguard;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+    private static final String KEY_PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+    private SwitchPreference mPrivacyGuardDefault;
+    private SwitchPreference mPrivacyGuardNotification;
+
+    public static PrivacyGuardPrefs newInstance() {
+        PrivacyGuardPrefs privacyGuardFragment = new PrivacyGuardPrefs();
+        return privacyGuardFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+
+        mPrivacyGuardDefault = (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardDefault.setChecked(false);
+        }
+
+        mPrivacyGuardNotification =
+            (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_NOTIFICATION);
+        mPrivacyGuardNotification.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardNotification.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardNotification.setChecked(true);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPrivacyGuardNotification) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}