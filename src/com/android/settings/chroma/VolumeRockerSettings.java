a6bcca3c48e861d7671fa3f717f21ee4553a6f7c
zephiK <zephik@gmail.com>
2015-10-14 00:06:12 -0400

    Volume Rocker Wake [2/2] Based on Rascarlo's code - https://github.com/rascarlo/android_packages_apps_settings/commit/33af4f0d419533bcb62ae16ea46bf3c134bf12ca

--- src/com/android/settings/chroma/VolumeRockerSettings.java
+++ src/com/android/settings/chroma/VolumeRockerSettings.java
@@ -15,20 +15,33 @@ import com.android.settings.SettingsPreferenceFragment;
 public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-		// private variables here
+    private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 		
+    private SwitchPreference mVolumeRockerWake;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.chroma_settings_volume);
+
+        // volume rocker wake
+        mVolumeRockerWake = (SwitchPreference) findPreference(VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setOnPreferenceChangeListener(this);
+        int volumeRockerWake = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_WAKE, 0);
+        mVolumeRockerWake.setChecked(volumeRockerWake != 0);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
-		// preference changes here
-        return false;
+        if (preference == mVolumeRockerWake) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+       }
+        return true;
     }
 
     @Override
