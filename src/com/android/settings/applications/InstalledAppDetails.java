bc28a08fc022fa7200d247ff7fcaa741fe9bb5fa
chirayudesai <chirayudesai1@gmail.com>
2015-10-21 01:32:12 +0300

    Show application's package name in the "App info" screen.
    * Adapted for marshmallow by @KreAch3R.

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -442,7 +442,7 @@ public class InstalledAppDetails extends AppInfoBase
     private void setAppLabelAndIcon(PackageInfo pkgInfo) {
         final View appSnippet = mHeader.findViewById(R.id.app_snippet);
         mState.ensureIcon(mAppEntry);
-        setupAppSnippet(appSnippet, mAppEntry.label, mAppEntry.icon,
+        setupAppSnippet(appSnippet, mAppEntry.label, mAppEntry.icon, pkgInfo.packageName,
                 pkgInfo != null ? pkgInfo.versionName : null);
     }
 
@@ -761,7 +761,7 @@ public class InstalledAppDetails extends AppInfoBase
         return true;
     }
 
-    public static void setupAppSnippet(View appSnippet, CharSequence label, Drawable icon,
+    public static void setupAppSnippet(View appSnippet, CharSequence label, Drawable icon, CharSequence packageName,
             CharSequence versionName) {
         LayoutInflater.from(appSnippet.getContext()).inflate(R.layout.widget_text_views,
                 (ViewGroup) appSnippet.findViewById(android.R.id.widget_frame));
@@ -770,7 +770,7 @@ public class InstalledAppDetails extends AppInfoBase
         iconView.setImageDrawable(icon);
 
         // Clicking on application icon opens application.
-        final String finalPackageName = pkgInfo.packageName;
+	final String finalPackageName = String.valueOf(packageName);
         iconView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -786,6 +786,18 @@ public class InstalledAppDetails extends AppInfoBase
         // Set application name.
         TextView labelView = (TextView) appSnippet.findViewById(android.R.id.title);
         labelView.setText(label);
+        
+	    // Set application package name.
+        TextView packageNameView = (TextView) appSnippet.findViewById(R.id.pkgname);
+        
+        if (!TextUtils.isEmpty(packageName)) {
+            packageNameView.setSelected(true);
+            packageNameView.setVisibility(View.VISIBLE);
+            packageNameView.setText(packageName);
+        } else {
+            packageNameView.setVisibility(View.INVISIBLE);
+        }
+        
         // Version number of application
         TextView appVersion = (TextView) appSnippet.findViewById(R.id.widget_text1);
 
