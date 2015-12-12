6d12d6ff7eb9942e7bb49a36d130a4b71b15dd24
Roman Birg <roman@cyngn.com>
2015-12-12 02:38:40 -0500

    Properly name string resources and update icon references

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -52,47 +52,47 @@ public class QSTileHolder {
                 break;
             case QSConstants.TILE_WIFI:
                 resourceName = "ic_qs_wifi_full_4";
-                stringId = R.string.wifi_quick_toggle_title;
+                stringId = R.string.qs_tile_wifi;
                 break;
             case QSConstants.TILE_BLUETOOTH:
                 resourceName = "ic_qs_bluetooth_on";
-                stringId = R.string.bluetooth_settings_title;
+                stringId = R.string.qs_tile_bluetooth;
                 break;
             case QSConstants.TILE_DND:
                 resourceName = "ic_qs_dnd_on";
-                stringId = R.string.quick_settings_dnd_label;
+                stringId = R.string.qs_tile_dnd;
                 break;
             case QSConstants.TILE_INVERSION:
-                resourceName = "ic_qs_inversion_on";
-                stringId = R.string.accessibility_display_inversion_preference_title;
+                resourceName = "ic_invert_colors_disable";
+                stringId = R.string.qs_tile_display_inversion;
                 break;
             case QSConstants.TILE_CELLULAR:
                 resourceName = "ic_qs_signal_full_4";
-                stringId = R.string.cellular_data_title;
+                stringId = R.string.qs_tile_cellular_data;
                 break;
             case QSConstants.TILE_AIRPLANE:
-                resourceName = "ic_qs_airplane_on";
-                stringId = R.string.airplane_mode;
+                resourceName = "ic_signal_airplane_disable";
+                stringId = R.string.qs_tile_airplane_mode;
                 break;
             case QSConstants.TILE_ROTATION:
-                resourceName = "ic_qs_rotation_locked";
-                stringId = R.string.display_rotation_title;
+                resourceName = "ic_portrait_to_auto_rotate";
+                stringId = R.string.qs_tile_display_rotation;
                 break;
             case QSConstants.TILE_FLASHLIGHT:
-                resourceName = "ic_qs_flashlight_on";
-                stringId = R.string.power_flashlight;
+                resourceName = "ic_signal_flashlight_disable";
+                stringId = R.string.qs_tile_flashlight;
                 break;
             case QSConstants.TILE_LOCATION:
                 resourceName = "ic_qs_location_on";
-                stringId = R.string.location_title;
+                stringId = R.string.qs_tile_location;
                 break;
             case QSConstants.TILE_CAST:
                 resourceName = "ic_qs_cast_on";
-                stringId = R.string.cast_screen;
+                stringId = R.string.qs_tile_cast_screen;
                 break;
             case QSConstants.TILE_HOTSPOT:
-                resourceName = "ic_qs_hotspot_on";
-                stringId = R.string.hotspot;
+                resourceName = "ic_hotspot_disable";
+                stringId = R.string.qs_tile_hotspot;
                 break;
             default:
                 return null;
