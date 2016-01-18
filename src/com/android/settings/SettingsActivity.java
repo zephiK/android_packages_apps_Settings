SAOSP-L/platform_packages_apps_Settings

QS: Longclick action [2/2]
* Long press expanded status bar Settings button opens tiles settings
* Fix Issue: Longpress opening settings app and QS tiles settings both.

src/com/android/settings/SettingsActivity.java
@@ -77,6 +77,7 @@ import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.simpleaosp.DisplayRotation;
 import com.android.settings.simpleaosp.AdditionalSettings;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.headsup.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -313,7 +314,8 @@ public class SettingsActivity extends Activity
             BlacklistSettings.class.getName(),         
             DisplayRotation.class.getName(),          
             ExpandedDesktopPreferenceFragment.class.getName(),
-	    HeadsUpSettings.class.getName()
+	    HeadsUpSettings.class.getName(),
+ 	    QSTiles.class.getName()
     };
 
 

zephiK/android_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -82,6 +82,7 @@ import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.chroma.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -359,7 +360,8 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             DisplayRotation.class.getName(),          
             BlacklistSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            QSTiles.class.getName()
     };
 
 